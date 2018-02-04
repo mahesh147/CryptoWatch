6d5b374b21dd94f669afcc85f0184f54c489e99a
mahesh147 <maheshchandran147@gmail.com>
2018-02-04 18:16:39 +0530

    Updated module.ts with the new the components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,22 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { AlertModule } from 'ngx-bootstrap';
 import { RouterModule } from '@angular/router';
+import { SimpleTimer } from 'ng2-simple-timer';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { BitcoinComponent } from './bitcoin/bitcoin.component';
-import { EthereumComponent } from './ethereum/ethereum.component';
+import { BitcoinLivePriceService } from './bitcoin/bitcoin-live-price.service';
 import { appRoutes } from '../routes';
+import { RippleComponent } from './ripple/ripple.component';
+import { RippleLivePriceService } from './ripple/ripple-live-price.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     BitcoinComponent,
-    EthereumComponent
+    RippleComponent
   ],
   imports: [
     BrowserModule,
@@ -25,7 +28,7 @@ import { appRoutes } from '../routes';
     AlertModule.forRoot(),
     RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  providers: [BitcoinLivePriceService, RippleLivePriceService, SimpleTimer],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
