467dfb285a88743d665ee331a6b33fa1b0d62f1b
mahesh147 <maheshchandran147@gmail.com>
2018-02-04 18:20:40 +0530

    Added a routing feature for the app

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,11 @@
 import { Routes } from '@angular/router';
 import { BitcoinComponent } from './app/bitcoin/bitcoin.component';
-import { EthereumComponent } from './app/ethereum/ethereum.component';
+import { RippleComponent } from './app/ripple/ripple.component';
 import { HomeComponent } from './app/home/home.component';
 
 export const appRoutes:Routes = [
  { path: 'home', component: HomeComponent },
  { path: 'bitcoin', component: BitcoinComponent },
- { path: 'ethereum', component: EthereumComponent },
+ { path: 'ripple', component: RippleComponent },
  { path: '', redirectTo: '/home', pathMatch: 'full'},
 ];
