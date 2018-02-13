baf1b316660477682fd2e8e1cf5303ecdcd87bca
mahesh147 <maheshchandran147@gmail.com>
2018-02-13 16:04:27 +0530

    Added Email-Password signup [alpha]

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase/app';
+import { Observable } from 'rxjs/Observable';
+
+@Injectable()
+export class AuthService {
+
+    private user: Observable<firebase.User>;
+
+  constructor(private _firebaseAuth: AngularFireAuth, private router: Router) { 
+
+    this.user = _firebaseAuth.authState;
+  }
+
+  signInRegular(email, password) {
+    const credential = firebase.auth.EmailAuthProvider.credential( email, password );
+
+    return this._firebaseAuth.auth.createUserWithEmailAndPassword(email, password);
+  }
+}
